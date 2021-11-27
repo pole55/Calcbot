a19862b5f08c5d729d7219819e9f65bd95450a76
Polina Horodyska <polkagor@ukr.net>
2021-11-27 16:03:28 +0200

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-from scipy.io import wavfile as wav
-from scipy.fftpack import fft
-import numpy as np
-rate, data = wav.read('sample_1.wav')
-fft_out = fft(data)
-# %matplotlib inline
-plt.plot(data, np.abs(fft_out))
-plt.show()
